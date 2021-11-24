e23b10dea981fbeab2f19a73b1452cacc8741e50
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-24 15:53:26 -0600

    final changes

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-/* 
-    1. 
-        Use fetch() to load data from photos.json
-        
-        Display the photos inside of a FlexBox container
-            The src will be https://picsum.photos/id/${photo.id}/100/100
-            The alt text will be the photo.title from the json
-
-        Use Flexbox to display the photos in a Row 
-            The Row should Wrap to a new line on overflow
-            Give each photo a white 0.1rem solid border
-            Rounded corners at 0.7rem border-radius
-            Each photo should be at least 1rem away from the photos around it
-*/
-
-
 async function getPhotos() {
     let response = await fetch("photos.json")
     let photos = await response.json()
     return photos
 }
 
-function getPhotosHtml(photos) {
+function getMyPhotosHtml(photos) {
     let myPhotosHtml = photos.map(photo => {
         return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
     }).join('')
-    console.log(myPhotosHtml)
-    
-    return `<div class="my-photos">${myPhotosHtml}</div>`   
+    return `<div class="my-photos">${myPhotosHtml}</div>`
 }
 
-getPhotos().then(photos => {    
+getPhotos().then(photos => {
+    let myPhotosHtml = getMyPhotosHtml(photos)
     document.body.innerHTML = `<div class="my-gallery">
-        <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
-        ${getPhotosHtml(photos)}
-    </div>`    
+        <img id="my-selected-photo" class="my-photo"
+        src="https://picsum.photos/id/${photos[2].id}/300/300" />
+        ${myPhotosHtml}
+    </div>`
     
     let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
     myPhotoImgs.forEach(photoImg => {
-        photoImg.addEventListener("click", event => {
-            let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`
-
+        photoImg.addEventListener("click", e => {
+            let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`
             let selectedPhoto = document.getElementById("my-selected-photo")
             selectedPhoto.src = selectedPhotoSrc
             selectedPhoto.style.display = "inline"
         })
-    })
+    })    
 })

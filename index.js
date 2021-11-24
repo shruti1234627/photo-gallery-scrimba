02f32df3c12a9cda0bc69387d600392219d3a81b
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-24 11:43:13 -0600

    photos rendering

--- index.js
+++ index.js
@@ -12,3 +12,18 @@
             Rounded corners at 0.7rem border-radius
             Each photo should be at least 1rem away from the photos around it
 */
+
+async function getPhotos() {
+    let response = await fetch("photos.json")
+    let photos = await response.json()
+    return photos
+}
+
+getPhotos().then(photos => {
+    let myPhotosHtml = photos.map(photo => {
+        return `<img src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
+    }).join('')
+    console.log(myPhotosHtml)
+    
+    document.body.innerHTML = `<div class="my-photos">${myPhotosHtml}</div>`
+})
